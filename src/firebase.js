ebc5bbf9e6039c61a8b6805dea5812243385c2c4
nuwan-dharmarathna <dharmarathna1220@gmail.com>
2025-05-09 07:58:03 +0530

    Add redux-persist package and update Login component

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -10,6 +9,4 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export default app;
+export const app = initializeApp(firebaseConfig);
